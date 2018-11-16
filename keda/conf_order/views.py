faa9c2b26577ba21d1068388ade367d77f5fceab
zhang_xiaode <zhang_xiaode@163.com>
2018-11-16 16:39:11 +0800

    会议预定页面修改，使用django templates

--- keda/conf_order/views.py
+++ keda/conf_order/views.py
@@ -7,4 +7,7 @@ def index(request):
     return render(request,'conf_order/index.html')
 
 def myorder(request):
-    return render(request,'conf_order/myorder.html')
+    time_splits_array = [['8:00-9:00','9:00-10:00','10:00-11:30'],
+            ['13:00-14:00','14:00-15:00','15:00-16:00'],
+            ['16:00:17:30','18:00-19:00','19:00-20:00']]
+    return render(request,'conf_order/myorder.html',{'time_splits_array':time_splits_array})
